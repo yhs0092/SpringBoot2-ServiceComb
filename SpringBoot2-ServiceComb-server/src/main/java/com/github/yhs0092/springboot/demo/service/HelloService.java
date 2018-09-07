62b17eb791689a55237e5c38e0a5090defb28fb6
yaohaishi <yaohaishi@huawei.com>
2018-09-07 18:18:52 +0800

    add server

---
+++ SpringBoot2-ServiceComb-server/src/main/java/com/github/yhs0092/springboot/demo/service/HelloService.java
@@ -0,0 +1,20 @@
+package com.github.yhs0092.springboot.demo.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(HelloService.class);
+
+  @RequestMapping(path = "/hello", method = RequestMethod.GET)
+  public String hello(@RequestParam(value = "name") String name) {
+    LOGGER.info("hello() is called, name = [{}]", name);
+    return "Hello, " + name;
+  }
+}
